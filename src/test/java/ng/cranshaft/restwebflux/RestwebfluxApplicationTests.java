a35c2bc4d7c7617cae159d22659cafdc431ca06f
Crankshaft <crankshatf@outlook.com>
2019-03-20 15:45:49 +0800

    Initial commit

---
+++ src/test/java/ng/cranshaft/restwebflux/RestwebfluxApplicationTests.java
@@ -0,0 +1,16 @@
+package ng.cranshaft.restwebflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestwebfluxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
